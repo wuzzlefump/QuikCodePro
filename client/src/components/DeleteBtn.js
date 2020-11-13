6c59884c71c3d8abe3de595e124c8bb6f19aeb0b
wuzzlefump <paulelliottwork@gmail.com>
2020-11-12 20:51:24 -0700

    delete button too

--- client/src/components/DeleteBtn.js
+++ client/src/components/DeleteBtn.js
@@ -1,7 +1,7 @@
 import React from "react";
-
+import Button from "react-bootstrap/Button"
 function DeleteBtn() {
-    return();
+    return(<Button variant="info">Delete</Button>);
 }
 
 export default DeleteBtn;
