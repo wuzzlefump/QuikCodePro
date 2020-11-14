76d31476b912728f373f6545feec8aa7d8a69653
Ben G <bensgar92@gmail.com>
2020-11-14 11:53:57 -0700

    added ace editor to newsnip page

--- client/src/pages/newsnip.js
+++ client/src/pages/newsnip.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import TheNav from '../components/thenav'
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import Editor from '../components/Editor'
 function NewSnip(){
 
     return(<>
     <TheNav/>
     {/* giant ace editor goes here */}
     <Jumbotron>
+        <Editor />
         {/* code title goes here */}
         {/* code type input goes here */}
         {/* keywords input goes here */}
