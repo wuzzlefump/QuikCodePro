73a90135d0dc378fc761c4f7cad1fdbe19e683df
Ben G <bensgar92@gmail.com>
2020-11-19 15:18:47 -0700

    added jokesgit add -A!

--- client/src/pages/signin.js
+++ client/src/pages/signin.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import SigninNav from '../components/signinnav'
 import SignInCard from '../components/signincard'
+import FooterPage from '../components/FooterPage'
 function SignIn(){
 
     return(<>
     <SigninNav/>
     <SignInCard/>
-    {/* jokes @ bottom */}
+    <FooterPage/>
     </>)
    
 }
