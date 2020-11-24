2d76fd4e3dcafd24a780c13fd25d68e15f5b922a
SpazCat <catherinelui.2@gmail.com>
2020-11-23 18:46:55 -0700

    Added scriptType in Mongo Schema to allow user to save more than one type of snippet. Added bio and avatar back to Users Schema

--- models/codes.js
+++ models/codes.js
@@ -1,21 +1,22 @@
-
-  
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const codeSchema = new Schema({
   title: { type: String, required: true },
   userId: { type: Number, required: true },
-  public: {type: Boolean, default: true},
+  public: { type: Boolean, default: true },
   codeType: { type: String, required: true },
-  snip: { type: String, required: true},
-  snipTwo: { type: String, required: false},
-  snipThree: { type: String, required: false},
-  keywords: {type: String, required: true},
+  snip: { type: String, required: true },
+  scriptType: { type: String, required: true },
+  snipTwo: { type: String, required: false },
+  scriptTypeTwo: { type: String, required: false },
+  snipThree: { type: String, required: false },
+  scripTypeThree: { type: String, required: false },
+  keywords: { type: String, required: true },
   dateCreated: { type: Date, default: Date.now },
-  comments: {type: String, required: false}
+  comments: { type: String, required: false },
 });
 
-const Codes = mongoose.model("Codes", codeSchema);
+const Codes = mongoose.model('Codes', codeSchema);
 
 module.exports = Codes;
