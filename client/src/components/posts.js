af4be49771f0e3ab0d7bc3bdc4d8c19574af80c5
Dr Phil <philip.prewitt@gmail.com>
2020-12-02 10:52:39 -0700

    styled the posts and added that feed array can only include public code

--- client/src/components/posts.js
+++ client/src/components/posts.js
@@ -7,18 +7,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function Posts({name, author, title, Public, snip, sniptwo, snipthree, language,
   languagetwo,languagethree,comments, userId, _id, keywords, avatar, updated}){
 
-    return(<Card className="my-1 py-0"style={{ width: '100%',  display:"flex", flexDirection:"column",  justifyContent:"center" }}>
+    return(<Card className="my-1"style={{ width: '100%',  display:"flex", flexDirection:"column",  justifyContent:"center" }}>
  
-    <CardBody>
-    <FontAwesomeIcon icon={avatar} size="3x" className = "mt-2 mr-3 float-left"></FontAwesomeIcon>
-      <CardText>
-       {author} Created a code snippet Titled {name} click the button to visit it
-      </CardText>
-      <AceModalGlobal name={title} author={author} language={language} 
+    <CardBody className=" ">
+    <FontAwesomeIcon icon={avatar} size="3x" className = "my-auto mr-4 float-left"></FontAwesomeIcon>
+      <p className="float-left mr-3 mt-3">
+       <b>{author} </b>  created a code snippet: 
+       </p>
+       <div className="pt-2">
+       <AceModalGlobal className="d-inline  " name={title} author={author} language={language} 
       snip={snip} avatar={avatar} comments={comments} title={title} 
       Public={Public} sniptwo= {sniptwo} snipthree={snipthree} 
       languagetwo={languagetwo} languagethree={languagethree} userId={userId}
       _id={_id} keywords={keywords} updated={updated}/>
+      </div>
+      
     </CardBody>
   </Card>)
 }
